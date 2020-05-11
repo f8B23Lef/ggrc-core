8bd724f323cd58d9d0c397ed14061e3471d6f4bf
natalya-mihalyonok <natallia_mikhalenak@epam.com>
2020-05-11 14:00:51 +0300

    Move logic of assessment-bulk-complete-table to container

--- src/ggrc-client/js/components/assessment/assessments-bulk-complete-container/assessments-bulk-complete-container.js
+++ src/ggrc-client/js/components/assessment/assessments-bulk-complete-container/assessments-bulk-complete-container.js
@@ -12,7 +12,10 @@ import {request} from '../../../plugins/utils/request-utils';
 import {getFiltersForCompletion} from '../../../plugins/utils/bulk-update-service';
 import {buildParam} from '../../../plugins/utils/query-api-utils';
 import {isMyAssessments} from '../../../plugins/utils/current-page-utils';
-import '../assessments-bulk-complete-table/assessments-bulk-complete-table';
+import '../assessments-bulk-complete-table/assessments-bulk-complete-table-header/assessments-bulk-complete-table-header';
+import '../assessments-bulk-complete-table/assessments-bulk-complete-table-row/assessments-bulk-complete-table-row';
+import '../../required-info-modal/required-info-modal';
+import {getCustomAttributeType} from '../../../plugins/utils/ca-utils';
 
 const ViewModel = canDefineMap.extend({
   currentFilter: {
@@ -53,6 +56,27 @@ const ViewModel = canDefineMap.extend({
   countAssessmentsToComplete: {
     value: 0,
   },
+  headersData: {
+    value: () => [],
+  },
+  rowsData: {
+    value: () => [],
+  },
+  requiredInfoModal: {
+    value: () => ({
+      title: '',
+      state: {
+        open: false,
+      },
+      content: {
+        attribute: null,
+        requiredInfo: null,
+        commentValue: null,
+        urls: [],
+        files: [],
+      },
+    }),
+  },
   buildAsmtListRequest() {
     let relevant = null;
     if (!isMyAssessments()) {
@@ -78,6 +102,114 @@ const ViewModel = canDefineMap.extend({
     this.attributesList = attributes;
     this.isLoading = false;
     this.isDataLoaded = true;
+    this.headersData = this.buildHeadersData();
+    this.rowsData = this.buildRowsData();
+  },
+  buildHeadersData() {
+    return this.attributesList.map((attribute) => ({
+      title: attribute.title,
+      mandatory: attribute.mandatory,
+    }));
+  },
+  buildRowsData() {
+    const rowsData = [];
+
+    this.assessmentsList.forEach((assessment) => {
+      const assessmentData = {
+        asmtId: assessment.id,
+        asmtTitle: assessment.title,
+        asmtStatus: assessment.status,
+        asmtType: assessment.assessment_type,
+        isReadyToComplete: false,
+      };
+      const attributesData = [];
+
+      this.attributesList.forEach((attribute) => {
+        let id = null;
+        let value = null;
+        let optionsList = [];
+        let optionsConfig = new Map();
+        let isApplicable = false;
+        const type = getCustomAttributeType(attribute.attribute_type);
+        const defaultValue = this.prepareAttributeValue(type,
+          attribute.default_value);
+
+        const assessmentAttributeData = attribute.values[assessment.id];
+        if (assessmentAttributeData) {
+          id = assessmentAttributeData.attribute_definition_id;
+          value = this.prepareAttributeValue(type,
+            assessmentAttributeData.value,
+            assessmentAttributeData.attribute_person_id);
+          ({optionsList, optionsConfig} = this.prepareMultiChoiceOptions(
+            assessmentAttributeData.multi_choice_options,
+            assessmentAttributeData.multi_choice_mandatory)
+          );
+          isApplicable = true;
+        }
+
+        attributesData.push({
+          id,
+          type,
+          value,
+          defaultValue,
+          isApplicable,
+          title: attribute.title,
+          mandatory: attribute.mandatory,
+          multiChoiceOptions: {
+            values: optionsList,
+            config: optionsConfig,
+          },
+          attachments: null,
+          modified: false,
+          validation: {
+            mandatory: attribute.mandatory,
+            valid: (isApplicable ? !attribute.mandatory : true),
+            requiresAttachment: false,
+            hasMissingInfo: false,
+          },
+        });
+      });
+
+      rowsData.push({attributes: attributesData, ...assessmentData});
+    });
+
+    return rowsData;
+  },
+  prepareAttributeValue(type, value, personId = null) {
+    switch (type) {
+      case 'checkbox':
+        return value === '1';
+      case 'date':
+        return value || null;
+      case 'dropdown':
+        return value || '';
+      case 'multiselect':
+        return value || '';
+      case 'person':
+        return personId
+          ? [{
+            id: personId,
+            type: 'Person',
+            href: `/api/people/${personId}`,
+            context_id: null,
+          }]
+          : null;
+      default:
+        return value;
+    }
+  },
+  prepareMultiChoiceOptions(multiChoiceOptions, multiChoiceMandatory) {
+    const optionsList = this.convertToArray(multiChoiceOptions);
+    const optionsStates = this.convertToArray(multiChoiceMandatory);
+    const optionsConfig = optionsStates.reduce((config, state, index) => {
+      const optionValue = optionsList[index];
+      return config.set(optionValue, Number(state));
+    }, new Map());
+
+    return {optionsList, optionsConfig};
+  },
+  convertToArray(value) {
+    return typeof value === 'string' ? value.split(',') : [];
   },
 });
 
--- src/ggrc-client/js/components/assessment/assessments-bulk-complete-container/tests/assessments-bulk-complete-container_spec.js
+++ src/ggrc-client/js/components/assessment/assessments-bulk-complete-container/tests/assessments-bulk-complete-container_spec.js
@@ -10,6 +10,7 @@ import * as RequestUtils from '../../../../plugins/utils/request-utils';
 import * as BulkUpdateService from '../../../../plugins/utils/bulk-update-service';
 import * as CurrentPageUtils from '../../../../plugins/utils/current-page-utils';
 import * as QueryApiUtils from '../../../../plugins/utils/query-api-utils';
+import * as CaUtils from '../../../../plugins/utils/ca-utils';
 
 describe('assessments-bulk-complete-container component', () => {
   let viewModel;
@@ -64,6 +65,8 @@ describe('assessments-bulk-complete-container component', () => {
   describe('loadItems() method', () => {
     beforeEach(() => {
       viewModel.asmtListRequest = new canMap();
+      spyOn(viewModel, 'buildHeadersData');
+      spyOn(viewModel, 'buildRowsData');
     });
 
     it('sets "isLoading" attr to true before request', () => {
@@ -113,6 +116,232 @@ describe('assessments-bulk-complete-container component', () => {
 
     beforeAll(() => {
       events = Component.prototype.events;
+    });
+
+    describe('buildHeadersData() method', () => {
+      it('returns formed array for headers data', () => {
+        viewModel.attributesList = [{
+          title: 'Title1',
+          mandatory: true,
+          default_value: 1,
+        }, {
+          title: 'Title2',
+          mandatory: false,
+          default_value: '',
+        }];
+
+        expect(viewModel.buildHeadersData().serialize()).toEqual([{
+          title: 'Title1',
+          mandatory: true,
+        }, {
+          title: 'Title2',
+          mandatory: false,
+        }]);
+      });
+    });
+
+    describe('buildRowsData() method', () => {
+      it('returns formed array for rows data', () => {
+        viewModel.assessmentsList = [{
+          id: 1,
+          title: 'Asmt1',
+          status: 'In Progress',
+          assessment_type: 'Control',
+        }, {
+          id: 2,
+          title: 'Asmt2',
+          status: 'Not Started',
+          assessment_type: 'Vendor',
+        }];
+
+        viewModel.attributesList = [{
+          attribute_type: 'Text',
+          title: 'LCA Text',
+          mandatory: false,
+          default_value: '',
+          values: {
+            '1': {
+              attribute_person_id: null,
+              attribute_definition_id: 4483,
+              value: 'Some text',
+              multi_choice_options: '',
+              multi_choice_mandatory: null,
+              definition_id: 1,
+            },
+          },
+        }];
+
+        spyOn(CaUtils, 'getCustomAttributeType').and.returnValue('input');
+
+        spyOn(viewModel, 'prepareAttributeValue')
+          .withArgs('input', '')
+          .and.returnValue('')
+          .withArgs('input', 'Some text', null)
+          .and.returnValue('Some text');
+
+        spyOn(viewModel, 'prepareMultiChoiceOptions')
+          .withArgs('', null)
+          .and.returnValue({
+            optionsList: [],
+            optionsConfig: new Map(),
+          });
+
+        expect(viewModel.buildRowsData()).toEqual([{
+          asmtId: 1,
+          asmtTitle: 'Asmt1',
+          asmtStatus: 'In Progress',
+          asmtType: 'Control',
+          attributes: [{
+            id: 4483,
+            type: 'input',
+            value: 'Some text',
+            defaultValue: '',
+            isApplicable: true,
+            title: 'LCA Text',
+            mandatory: false,
+            multiChoiceOptions: {
+              values: [],
+              config: new Map(),
+            },
+            attachments: null,
+            modified: false,
+            validation: {
+              mandatory: false,
+              valid: true,
+              requiresAttachment: false,
+              hasMissingInfo: false,
+            },
+          }],
+        }, {
+          asmtId: 2,
+          asmtTitle: 'Asmt2',
+          asmtStatus: 'Not Started',
+          asmtType: 'Vendor',
+          attributes: [{
+            id: null,
+            type: 'input',
+            value: null,
+            defaultValue: '',
+            isApplicable: false,
+            title: 'LCA Text',
+            mandatory: false,
+            multiChoiceOptions: {
+              values: [],
+              config: new Map(),
+            },
+            attachments: null,
+            modified: false,
+            validation: {
+              mandatory: false,
+              valid: true,
+              requiresAttachment: false,
+              hasMissingInfo: false,
+            },
+          }],
+        }]);
+      });
+    });
+
+    describe('prepareAttributeValue() method', () => {
+      describe('if type is "checkbox"', () => {
+        it('should return "true"', () => {
+          expect(viewModel.prepareAttributeValue('checkbox', '1')).toBe(true);
+        });
+
+        it('should return "false"', () => {
+          expect(viewModel.prepareAttributeValue('checkbox', '0')).toBe(false);
+        });
+      });
+
+      describe('if type is "date"', () => {
+        it('should return value', () => {
+          expect(viewModel.prepareAttributeValue('date', '2020/04/15'))
+            .toBe('2020/04/15');
+        });
+
+        it('should return "null"', () => {
+          expect(viewModel.prepareAttributeValue('date', '')).toBeNull();
+        });
+      });
+
+      describe('if type is "dropdown"', () => {
+        it('should return value', () => {
+          expect(viewModel.prepareAttributeValue('dropdown', '1,2,3'))
+            .toBe('1,2,3');
+        });
+
+        it('should return ""', () => {
+          expect(viewModel.prepareAttributeValue('dropdown', '')).toBe('');
+        });
+      });
+
+      describe('if type is "multiselect"', () => {
+        it('should return value', () => {
+          expect(viewModel.prepareAttributeValue('multiselect', '1,2,3'))
+            .toBe('1,2,3');
+        });
+
+        it('should return ""', () => {
+          expect(viewModel.prepareAttributeValue('multiselect', '')).toBe('');
+        });
+      });
+
+      describe('if type is "person"', () => {
+        it('should return formed array with needed data about person', () => {
+          expect(viewModel.prepareAttributeValue('person', null, 384))
+            .toEqual([{
+              id: 384,
+              type: 'Person',
+              href: '/api/people/384',
+              context_id: null,
+            }]);
+        });
+
+        it('should return "null"', () => {
+          expect(viewModel.prepareAttributeValue('person', null, null))
+            .toBeNull();
+        });
+      });
+
+      describe('for default block', () => {
+        it('should return value', () => {
+          expect(viewModel.prepareAttributeValue('input', 'Some text'))
+            .toBe('Some text');
+        });
+      });
+    });
+
+    describe('prepareMultiChoiceOptions() method', () => {
+      beforeEach(() => {
+        spyOn(viewModel, 'convertToArray')
+          .and.returnValues(['1', '2', '3'], ['2', '4', '1']);
+      });
+
+      it('calls convertToArray()', () => {
+        viewModel.prepareMultiChoiceOptions('1,2,3', '2,4,1');
+
+        expect(viewModel.convertToArray).toHaveBeenCalledTimes(2);
+        expect(viewModel.convertToArray.calls.argsFor(0)).toEqual(['1,2,3']);
+        expect(viewModel.convertToArray.calls.argsFor(1)).toEqual(['2,4,1']);
+      });
+
+      it('returns formed optionsList and optionsConfig', () => {
+        expect(viewModel.prepareMultiChoiceOptions('1,2,3', '2,4,1'))
+          .toEqual({
+            optionsList: ['1', '2', '3'],
+            optionsConfig: new Map([['1', 2], ['2', 4], ['3', 1]]),
+          });
+      });
+    });
+
+    describe('convertToArray() method', () => {
+      it('returns array of values if parameter is "string" type', () => {
+        expect(viewModel.convertToArray('1,2,3')).toEqual(['1', '2', '3']);
+      });
+
+      it('returns empty array if parameter is not "string" type', () => {
+        expect(viewModel.convertToArray(123)).toEqual([]);
+      });
     });
 
     describe('inserted() method', () => {
